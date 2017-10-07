83d8d367cda5cee3d2270c702cb7447d3790004e
Seven Du <shiweidu@outlook.com>
2017-10-07 18:14:36 +0800

    抽离 with nav 样式。

--- resources/assets/components/WithNavigation.js
+++ resources/assets/components/WithNavigation.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import withWidth from 'material-ui/utils/withWidth';
 import Navigation from './common/Navigation';
+import style from './WithNavigation.style';
 
 class WithNavigation extends Component {
 
   static propTypes = {
     width: PropTypes.string.isRequired,
+    classes: PropTypes.object.isRequired,
+    tab: PropTypes.string.isRequired,
   };
 
   render() {
-    const { children, width } = this.props;
+    const { children, width, classes, tab } = this.props;
 
     return (
-      <div>
-        <Navigation width={width}></Navigation>
+      <div className={classNames(classes.root, { [classes.xs]: width === 'xs', [classes.high]: width !== 'xs' })}>
+        <Navigation width={width} tab={tab}></Navigation>
         { children }
       </div>
     );
   }
 }
 
-export default withWidth()(WithNavigation);
+export default withWidth()(
+  style(WithNavigation)
+);
---
+++ resources/assets/components/WithNavigation.style.js
@@ -0,0 +1,11 @@
+import withStyles from 'material-ui/styles/withStyles';
+export default withStyles(() => ({
+  root: {
+    minHeight: '100vh',
+    boxSizing: 'border-box',
+  },
+  xs: {
+    paddingBottom: 56,
+  },
+  high: {},
+}));
