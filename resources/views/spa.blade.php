5ca11327340c5047d040d8441f451ff586865c4e
Seven Du <shiweidu@outlook.com>
2017-10-08 17:37:47 +0800

    增加接口设置。

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -5,6 +5,11 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <title>{{ config('app.name') }}</title>
+
+  <!-- Fans setting. -->
+  <meta name="api-base" content="{{ url('/api') }}">
+  <meta name="api-version" content="v1">
+
 </head>
 <body>
 <div id="app"></div>
