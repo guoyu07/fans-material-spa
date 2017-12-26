badbd4ef7d7c9af957ba97715a84c2e1993d0bac
Seven Du <shiweidu@outlook.com>
2017-12-26 23:52:19 +0800

    chore: Deleted old code.

--- old/components/common/Navigation.style.js
+++
@@ -1,8 +0,0 @@
-import withStyles from 'material-ui/styles/withStyles';
-export default withStyles(() => ({
-  xsRoot: {
-    width: '100%',
-    top: 'unset',
-    bottom: 0,
-  },
-}));
--- old/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
-import * as reducers from './reducers';
-
-export default createStore(
-  combineReducers(reducers),
-  applyMiddleware(thunk)
-);
