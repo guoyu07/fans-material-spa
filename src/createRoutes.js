4b91316d651da12816a60176b79592f2ef2c9b81
Seven Du <shiweidu@outlook.com>
2018-02-13 11:36:54 +0800

    重新整理路由

--- src/createRoutes.js
+++ src/createRoutes.js
@@ -1,23 +1,22 @@
 // @flow
 
-// 先注释，暂时不用切分代码
 import asyncComponent from './utils/asyncComponent';
 
-import HomePage from './pages/Home'; // asyncComponent(() => import('./pages/Home'))
-import ForumPage from './pages/Forum'; // asyncComponent(() => import('./pages/Forum'))
-import TagPage from './pages/Tag'; // asyncComponent(() => import('./pages/Tag'))
-import PersonPage from './pages/Person'; // asyncComponent(() => import('./pages/Person'))
-import LoginPage from './pages/Login';
+const asyncHome = asyncComponent(() => import('./pages/home'));
+const asyncForum = asyncComponent(() => import('./pages/forum'));
+const asyncTag = asyncComponent(() => import('./pages/tag'));
+const asyncPerson = asyncComponent(() => import('./pages/person'));
+const asyncLogin = asyncComponent(() => import('./pages/login'));
 
 const routes = [
-  { path: '/', exact: true, component: HomePage },
-  { path: '/forums', exact: true, component: ForumPage },
-  { path: '/tags', exact: true, component: TagPage },
-  { path: '/person', exact: true, component: PersonPage },
-  { path: '/login', component: LoginPage },
+  { path: '/', exact: true, component: asyncHome },
+  { path: '/forums', exact: true, component: asyncForum },
+  { path: '/tags', exact: true, component: asyncTag },
+  { path: '/person', exact: true, component: asyncPerson },
+  { path: '/login', component: asyncLogin },
 
   // Finally, catch all unmatched routes
-  { component: asyncComponent(() => import('./pages/NotFound')) },
+  { component: asyncComponent(() => import('./pages/not-found')) },
 ];
 
 export default routes;
