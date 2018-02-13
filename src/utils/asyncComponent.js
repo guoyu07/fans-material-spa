bd3decf1e0675035a19a618e7c44005f13f83783
Seven Du <shiweidu@outlook.com>
2018-02-13 11:35:06 +0800

    增加异步组件支持！

--- src/utils/asyncComponent.js
+++ src/utils/asyncComponent.js
@@ -1,43 +1,12 @@
 // @flow
 
-import * as React from 'react';
+import Loadable from 'react-loadable';
+import loading from '../modules/module-loading';
 
-export default function (module: Function) {
-  class AsyncComponent extends React.Component<any, any> {
-
-    /**
-     * The component state.
-     *
-     * @type {Object}
-     */
-    state = {
-      component: null,
-    }
-
-    /**
-     * The async component render.
-     *
-     * @return {Reqact.Node}
-     * @author Seven Du <shiweidu@outlook.com>
-     */
-    render(): React.Node {
-      const Component = this.state.component;
-
-      return Component ? <Component {...this.props} /> : null;
-    }
-
-    /**
-     * The cmponent did mount async.
-     *
-     * @return {void}
-     * @author Seven Du <shiweidu@outlook.com>
-     */
-    async componentDidMount() {
-      const { default: component } = await module();
-
-      this.setState({ component });
-    }
-  }
-
-  return AsyncComponent;
+export default function (module: Function, options: Object = {}): Function {
+  return Loadable({
+    loading: loading,
+    ...options,
+    loader: module,
+  });
 };
